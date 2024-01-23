35e354ae6e5ef76d1c03d56e15b154907b2664ef
Ahmed Emin <126208272+AEmin96@users.noreply.github.com>
2024-01-23 21:45:15 +0000

    Add jQuerry code to change the active nav elements dinamically

--- home/views.py
+++ home/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/index.html')
+
+def aboutus(request):
+    return render(request, 'home/aboutus.html')
+
+def location(request):
+    return render(request, 'home/location.html')
+
+def testimonials(request):
+    return render(request, 'home/testimonials.html')
