f76f1890dffa4a1b647a7958b2c2a57e6eba3920
AEmin96 <maxxseed00@yahoo.com>
2024-02-12 08:31:17 +0000

    Create models for the checkout section

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Checkout(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.subscription.subscription_title} - {self.amount_paid}"
