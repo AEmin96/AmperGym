da30994c583e9414fa86f28770a1bc9aa6de1b96
AEmin96 <maxxseed00@yahoo.com>
2024-02-12 08:44:42 +0000

    Delete adapter.py from subscriptions as not needed

--- subscriptions/adapter.py
+++
@@ -1,6 +0,0 @@
-from allauth.account.adapter import DefaultAccountAdapter
-
-class MyAccountAdapter(DefaultAccountAdapter):
-    def get_login_redirect_url(self, request):
-        # Custom logic to return the redirect URL
-        return super(MyAccountAdapter, self).get_login_redirect_url(request)
