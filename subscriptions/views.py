dfc196fd4f39f97758f734ccadf4b57f3e490c4e
AEmin96 <maxxseed00@yahoo.com>
2024-02-12 08:36:48 +0000

    Add views for subscriptions

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.shortcuts import render
+from .models import UserSubscription,Subscription
 from django.contrib.auth.decorators import login_required
 
-def subscriptions(request): 
-    return render(request, 'subscriptions/subscriptions.html')
+@login_required
+def subscriptions(request):
+    subscriptions = Subscription.objects.all()
+    current_subscription = None
+    if UserSubscription.objects.filter(user=request.user).exists():
+        user_subscription = UserSubscription.objects.get(user=request.user)
+        current_subscription = user_subscription.subscription.subscription_type
+    
+    return render(request, 'subscriptions/subscriptions.html', {'current_subscription': current_subscription,'subscriptions':subscriptions})
