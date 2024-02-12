82bee62d2535175d55bf1d501f1bbf0d7c16d95f
AEmin96 <maxxseed00@yahoo.com>
2024-02-12 08:36:28 +0000

    Create models for subscriptions

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -3,34 +3,21 @@ from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
 class Subscription(models.Model):
-    class SubscriptionTypes(models.TextChoices):
-        BASIC = 'BA', _('Basic Subscription')
-        PREMIUM = 'PR', _('Premium Subscription')
-        ELITE = 'EL', _('Elite Subscription')
-
-    # Define a class attribute for subscription prices
-    SUBSCRIPTION_PRICES = {
-        'BA': 14.99,
-        'PR': 24.99,
-        'EL': 34.99,
-    }
-
-    user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='subscription'
-    )
-    subscription_type = models.CharField(
-        max_length=2,
-        choices=SubscriptionTypes.choices,
-        default=SubscriptionTypes.BASIC,
-    )
+    subscription_type = models.CharField(max_length=10, default='BA',blank= True)
+    subscription_title = models.CharField(max_length=100, default='Basic Subscription',blank= True)
+    subscription_desc = models.CharField(max_length=300 ,default='Access to basic gym facilities Personalized workout plans £14.99/month',blank= True)
+    subscription_price = models.FloatField(default=14.99)
     start_date = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.subscription_type}"
+
+
+class UserSubscription(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    subscription = models.ForeignKey(Subscription, on_delete=models.SET_NULL, null=True, blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True) 
 
     def __str__(self):
-        return f"{self.user.username}'s subscription"
-
-    @property
-    def price(self):
-        """Return the price of the current subscription."""
-        return self.SUBSCRIPTION_PRICES.get(self.subscription_type)
+        return f"{self.user.username} - {self.subscription.subscription_title}"
