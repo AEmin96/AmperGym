9e2e044ed35a11862323081528c50d4b7ff99915
AEmin96 <maxxseed00@yahoo.com>
2024-02-12 08:36:10 +0000

    Add subscriptions to admin panel

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Subscription
 
-# Register your models here.
+# Optional: Create a custom admin class to customize the admin interface
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('subscription_type', 'start_date')
+    search_fields = ('subscription_type',) 
+    list_filter = ('subscription_type',)
+
+# Register the Subscription model with the custom admin class
+admin.site.register(Subscription, SubscriptionAdmin)
