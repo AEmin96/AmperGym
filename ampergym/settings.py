93b384649b09098b86be4613a8dfe0006ce3ca54
AEmin96 <maxxseed00@yahoo.com>
2024-02-13 21:56:25 +0000

    add allowed host

--- ampergym/settings.py
+++ ampergym/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ampergym-f292fdd6def8.herokuapp.com']
+ALLOWED_HOSTS = ['ampergym-f292fdd6def8.herokuapp.com', 'www.ampergym-f292fdd6def8.herokuapp.com']
 
 # Application definition
 
