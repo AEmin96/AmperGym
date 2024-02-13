58d8a0671da0977dd9f10f6a32c61fe686d68fc3
AEmin96 <maxxseed00@yahoo.com>
2024-02-13 21:42:55 +0000

    Change allowed hosts

--- ampergym/settings.py
+++ ampergym/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-aemin96-ampergym-6qhobg0voni.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['ampergym-f292fdd6def8.herokuapp.com']
 
 # Application definition
 
