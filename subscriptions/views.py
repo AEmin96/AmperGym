1cb1b4fa264c2601b05604084e4439805a832fb1
AEmin96 <maxxseed00@yahoo.com>
2024-02-14 10:01:55 +0000

    Order the subscription types correctly

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
-from .models import UserSubscription,Subscription
+from .models import UserSubscription, Subscription
 from django.contrib.auth.decorators import login_required
+from django.db.models import Case, When
 
 @login_required
 def subscriptions(request):
-    subscriptions = Subscription.objects.all()
+    # Define custom ordering for subscriptions
+    custom_ordering = Case(
+        When(subscription_type='BA', then=1),
+        When(subscription_type='PR', then=2),
+        When(subscription_type='EL', then=3),
+    )
+    
+    subscriptions = Subscription.objects.all().annotate(
+        custom_order=custom_ordering
+    ).order_by('custom_order')
+    
     current_subscription = None
     if UserSubscription.objects.filter(user=request.user).exists():
         user_subscription = UserSubscription.objects.get(user=request.user)
         current_subscription = user_subscription.subscription.subscription_type
-    
-    return render(request, 'subscriptions/subscriptions.html', {'current_subscription': current_subscription,'subscriptions':subscriptions})
+
+    return render(request, 'subscriptions/subscriptions.html', {'current_subscription': current_subscription, 'subscriptions': subscriptions})
