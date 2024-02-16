8e7e1dd807f225e257f4f3013b83b14ce76ba5a9
AEmin96 <maxxseed00@yahoo.com>
2024-02-16 11:14:53 +0000

    Add a function to cancel current subscription

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import UserSubscription, Subscription
 from django.contrib.auth.decorators import login_required
 from django.db.models import Case, When
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
+
 
 @login_required
 def subscriptions(request):
@@ -22,3 +26,15 @@ def subscriptions(request):
         current_subscription = user_subscription.subscription.subscription_type
 
     return render(request, 'subscriptions/subscriptions.html', {'current_subscription': current_subscription, 'subscriptions': subscriptions})
+
+@login_required
+def cancel_subscription(request):
+    user_subscription_qs = UserSubscription.objects.filter(user=request.user)
+    if user_subscription_qs.exists():
+        user_subscription = user_subscription_qs.first()
+        user_subscription.delete()
+        messages.success(request, 'Your subscription has been successfully canceled.')
+    else:
+        messages.error(request, 'You do not have an active subscription to cancel.')
+    
+    return HttpResponseRedirect(reverse('subscriptions:subscriptions'))
