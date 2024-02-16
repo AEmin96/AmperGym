8eacef0385696db6ff0d9b50dafe6f6480e21823
AEmin96 <maxxseed00@yahoo.com>
2024-02-16 15:30:18 +0000

    Fix subscription views

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -8,7 +8,7 @@ from django.contrib import messages
 
 
 @login_required
-def subscriptions(request):
+def subscriptions_view(request):
     # Define custom ordering for subscriptions
     custom_ordering = Case(
         When(subscription_type='BA', then=1),
@@ -16,25 +16,27 @@ def subscriptions(request):
         When(subscription_type='EL', then=3),
     )
     
-    subscriptions = Subscription.objects.all().annotate(
+    subscription_list = Subscription.objects.all().annotate(
         custom_order=custom_ordering
     ).order_by('custom_order')
     
     current_subscription = None
-    if UserSubscription.objects.filter(user=request.user).exists():
-        user_subscription = UserSubscription.objects.get(user=request.user)
+    user_subscription = UserSubscription.objects.filter(user=request.user).first()
+    if user_subscription:
         current_subscription = user_subscription.subscription.subscription_type
 
-    return render(request, 'subscriptions/subscriptions.html', {'current_subscription': current_subscription, 'subscriptions': subscriptions})
+    return render(request, 'subscriptions/subscriptions.html', {
+        'current_subscription': current_subscription,
+        'subscriptions': subscription_list
+    })
 
 @login_required
 def cancel_subscription(request):
-    user_subscription_qs = UserSubscription.objects.filter(user=request.user)
-    if user_subscription_qs.exists():
-        user_subscription = user_subscription_qs.first()
+    user_subscription = UserSubscription.objects.filter(user=request.user).first()
+    if user_subscription:
         user_subscription.delete()
         messages.success(request, 'Your subscription has been successfully canceled.')
     else:
         messages.error(request, 'You do not have an active subscription to cancel.')
     
-    return HttpResponseRedirect(reverse('subscriptions:subscriptions'))
+    return HttpResponseRedirect(reverse('subscriptions:subscriptions_view'))
