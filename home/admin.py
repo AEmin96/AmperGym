8cff9b03b600699ca5eb202c0c77ebac6d5bd530
AEmin96 <maxxseed00@yahoo.com>
2024-03-25 19:35:16 +0000

    Add Newsletter column for admin for user

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import User,UserProfile
 # Register your models here.
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [UserProfileInline]
+    resource_class = User
+    list_display = ('id', 'username', 'last_login','email', 'first_name', 'last_name', 'is_active', 'is_staff','is_superuser')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+import os
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    newsletter = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
